7fc4395f8d421d01334cec0b01582068df63d260
Benoit Pesty <Benoit.Pesty@ign.fr>
2010-08-24 12:42:30 +0000

    DBIFN-83 : Ajout des requêtes prédéfinies

--- Bases SQL/__Predefined requests.sql
+++ Bases SQL/__Predefined requests.sql
@@ -25,6 +25,12 @@ COMMENT ON COLUMN PREDEFINED_REQUEST.REQUEST_NAME IS 'The request name';
 COMMENT ON COLUMN PREDEFINED_REQUEST.SCHEMA_CODE IS 'The schema used by this request';
 COMMENT ON COLUMN PREDEFINED_REQUEST.DATASET_ID IS 'The dataset used by this request';
 COMMENT ON COLUMN PREDEFINED_REQUEST.DEFINITION IS 'The description of the request';
+COMMENT ON COLUMN PREDEFINED_REQUEST.LABEL IS 'The label of the request';
+COMMENT ON COLUMN PREDEFINED_REQUEST.CLICK IS 'Count the number of clicks on the request';
+COMMENT ON COLUMN PREDEFINED_REQUEST.DATE IS 'Date of creation of the request';
+COMMENT ON COLUMN PREDEFINED_REQUEST.CRITERIA_HINT IS 'Help text that is displayed above the criterias';
+
+
 
 /*==============================================================*/
 /* Table : PREDEFINED_REQUEST_CRITERIA                          */
@@ -42,6 +48,7 @@ COMMENT ON COLUMN PREDEFINED_REQUEST_CRITERIA.REQUEST_NAME IS 'The request name'
 COMMENT ON COLUMN PREDEFINED_REQUEST_CRITERIA.FORMAT IS 'The form format of the criteria';
 COMMENT ON COLUMN PREDEFINED_REQUEST_CRITERIA.DATA IS 'The form field of the criteria';
 COMMENT ON COLUMN PREDEFINED_REQUEST_CRITERIA.VALUE IS 'The field value (multiple values are separated by a semicolon)';
+COMMENT ON COLUMN PREDEFINED_REQUEST_CRITERIA.FIXED IS 'Indicate if the file is fixed or selectable';
 
 ALTER TABLE ONLY predefined_request_criteria
     ADD CONSTRAINT fk_predefined_request_criteria_request_name 
@@ -58,9 +65,9 @@ DATA                   VARCHAR(36)          not null,
 constraint PK_PREDEFINED_REQUEST_RESULT primary key (REQUEST_NAME, FORMAT, DATA)
 );
 
-COMMENT ON COLUMN PK_PREDEFINED_REQUEST_RESULT.REQUEST_NAME IS 'The request name';
-COMMENT ON COLUMN PK_PREDEFINED_REQUEST_RESULT.FORMAT IS 'The form format of the result column';
-COMMENT ON COLUMN PK_PREDEFINED_REQUEST_RESULT.DATA IS 'The form field of the result column';
+COMMENT ON COLUMN PREDEFINED_REQUEST_RESULT.REQUEST_NAME IS 'The request name';
+COMMENT ON COLUMN PREDEFINED_REQUEST_RESULT.FORMAT IS 'The form format of the result column';
+COMMENT ON COLUMN PREDEFINED_REQUEST_RESULT.DATA IS 'The form field of the result column';
 
 ALTER TABLE ONLY predefined_request_result
     ADD CONSTRAINT fk_predefined_request_result_request_name 
@@ -71,23 +78,30 @@ ALTER TABLE ONLY predefined_request_result
 /*==============================================================*/
 /* Table : PREDEFINED_REQUEST_GROUP                             */
 /*==============================================================*/
-CREATE TABLE predefined_request_group (
+CREATE TABLE PREDEFINED_REQUEST_GROUP (
 GROUP_NAME 		VARCHAR(50) 	NOT NULL,
 LABEL			VARCHAR(50),
 DEFINITION  		VARCHAR(250),
 constraint PK_PREDEFINED_REQUEST_GROUP primary key (GROUP_NAME)
 );
 
+COMMENT ON COLUMN PREDEFINED_REQUEST_GROUP.GROUP_NAME IS 'The name of the group';
+COMMENT ON COLUMN PREDEFINED_REQUEST_GROUP.LABEL IS 'The label of the group';
+COMMENT ON COLUMN PREDEFINED_REQUEST_GROUP.DEFINITION IS 'The definition of the group';
+
 
 
 /*==============================================================*/
 /* Table : PREDEFINED_REQUEST_GROUP_ASSO                        */
 /*==============================================================*/
-CREATE TABLE predefined_request_group_asso (
+CREATE TABLE PREDEFINED_REQUEST_GROUP_ASSO (
 GROUP_NAME 		VARCHAR(50) 	NOT NULL,
 REQUEST_NAME 	VARCHAR(50),
 constraint PK_PREDEFINED_REQUEST_GROUP_ASSO primary key (GROUP_NAME, REQUEST_NAME)
 );
+
+COMMENT ON COLUMN PREDEFINED_REQUEST_GROUP_ASSO.GROUP_NAME IS 'The name of the group';
+COMMENT ON COLUMN PREDEFINED_REQUEST_GROUP_ASSO.REQUEST_NAME IS 'The label of the predefined request';
 
        
 ALTER TABLE ONLY predefined_request_group_asso
