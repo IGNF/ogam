9d2571abd61474c8cce55981b20aa4b27dc711d6
Benoit Pesty <Benoit.Pesty@ign.fr>
2012-02-06 14:49:57 +0000

    OGAM-162 : MapPanel : La gestion de la transparence des couches ne fonctionne plus

---
+++ website/htdocs/ogam/public/js/genapp/source/widgets/tree/ContextMenuPlugin.js
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2008-2009 The Open Source Geospatial Foundation
+ * 
+ * Published under the BSD license. See
+ * http://svn.geoext.org/core/trunk/geoext/license.txt for the full text of the
+ * license.
+ */
+
+// A supprimer quand sera intégré dans GeoExt
+Ext.namespace("Genapp.tree");
+
+Genapp.tree.ContextMenuPlugin = Ext.extend(Ext.util.Observable, {
+
+	/* begin i18n */
+	/** api: config[deleteLayerText] ``String`` i18n */
+	deleteLayerText : "Delete layer",
+
+	/** api: config[deleteLayerConfirmationText] ``String`` i18n */
+	deleteLayerConfirmationText : "Are you sure you wish to remove this layer: ",
+
+	/** api: config[changeOpacityText] ``String`` i18n */
+	changeOpacityText : "Change opacity",
+
+	/* end i18n */
+
+	sliderOptions : {},
+
+	defaultSliderOptions : {
+		width : 200
+	},
+
+	menu : null,
+
+	constructor : function(config) {
+		Ext.apply(this.initialConfig, Ext.apply({}, config));
+		Ext.apply(this, config);
+		Ext.applyIf(this.sliderOptions, this.defaultSliderOptions);
+		this.addEvents("contextmenu");
+		Genapp.tree.ContextMenuPlugin.superclass.constructor.apply(this, arguments);
+	},
+
+	init : function(tree) {
+		tree.on({
+			"contextmenu" : this.onContextMenu,
+			scope : this
+		});
+	},
+
+	onContextMenu : function(node, e) {
+		// if the node clicked has no layer, there is nothing to do.
+		if (!node.layer) {
+			return;
+		}
+
+		var sliderOptions, contextMenuItems = [], a;
+
+		e.stopEvent();
+
+		a = node.attributes;
+
+		if (this.menu) {
+			this.menu.destroy();
+		}
+
+		sliderOptions = Ext.applyIf({
+			layer : node.layer
+		}, this.sliderOptions);
+
+		// OpacitySlider
+		contextMenuItems.push({
+			text : this.changeOpacityText,
+			menu : {
+				plain : true,
+				items : [ new GeoExt.LayerOpacitySlider(sliderOptions) ]
+			}
+		});
+
+		// DeleteLayer
+		if (a.allowDelete) {
+			contextMenuItems.push({
+				text : this.deleteLayerText,
+				handler : function() {
+					Ext.MessageBox.confirm(this.deleteLayerText, this.deleteLayerConfirmationText + node.layer.name, function(btn) {
+						if (btn == 'yes') {
+							var store = this.layerStore;
+							var nRecords = store.getCount();
+
+							var layers = [];
+							for ( var i = 0; i < nRecords; i++) {
+								layers.push(store.getAt(i).get('layer'));
+							}
+
+							var layerIdx = OpenLayers.Util.indexOf(layers, this.layer);
+
+							if (layerIdx != -1) {
+								store.remove(store.getAt(layerIdx));
+							}
+						}
+					}, node);
+				},
+				scope : this
+			});
+		}
+
+		// ContextMenu
+		this.menu = new Ext.menu.Menu({
+			ignoreParentClick : true,
+			defaults : {
+				scope : node.getOwnerTree()
+			},
+			items : contextMenuItems
+		});
+
+		this.menu.showAt(e.getXY());
+	}
+
+});
+
+Ext.preg && Ext.preg("gx_tree_contextmenuplugin", Genapp.tree.ContextMenuPlugin);
--- website/htdocs/ogam/public/js/genapp/source/widgets/tree/LayerTreePanel.js
+++ website/htdocs/ogam/public/js/genapp/source/widgets/tree/LayerTreePanel.js
@@ -5,176 +5,214 @@
  * @class Genapp.tree.LayerTreePanel
  * @extends Ext.tree.TreePanel
  * @constructor Create a new LayerTreePanel
- * @param {Object} config
+ * @param {Object}
+ *            config
  * @xtype Ext.tree.TreePanel
  */
 
 Ext.namespace('Genapp.tree');
 
-Genapp.tree.LayerTreePanel = Ext.extend( Ext.tree.TreePanel, {
+Genapp.tree.LayerTreePanel = Ext.extend(Ext.tree.TreePanel, {
 
-    autoScroll : true,
-    rootVisible : false,
-    enableDD: true,
-    title : '',
-    border : false,
-    /**
-     * Read-only. An object containing the node id for each layer name. {layerName: nodeId, ...}
-     * @type Object
-     */
-    layerNodeIds: [],
+	autoScroll : true,
+	rootVisible : false,
+	enableDD : true,
+	title : '',
+	border : false,
+	/**
+	 * Read-only. An object containing the node id for each layer name.
+	 * {layerName: nodeId, ...}
+	 * 
+	 * @type Object
+	 */
+	layerNodeIds : [],
 
-    // private
-    initComponent: function(){
-        // Add a loader to the root children node config if needed
-        for(var i = 0; i< this.rootChildren.length; i++){
-            this.addLoaderToNodeConfig(this.rootChildren[i]);
-        }
-        // Create the tree root node
-        this.root = new Ext.tree.AsyncTreeNode({
-            children : this.rootChildren,
-            leaf: false,
-            expanded : true
-        });
-        // Toggle the children on the parent node 'checkchange' event
-        this.on('checkchange', this.toggleChildrenOnParentToggle, this);
+	// private
+	initComponent : function() {
+		// Add a loader to the root children node config if needed
+		for ( var i = 0; i < this.rootChildren.length; i++) {
+			this.addLoaderToNodeConfig(this.rootChildren[i]);
+		}
+		// Create the tree root node
+		this.root = new Ext.tree.AsyncTreeNode({
+			children : this.rootChildren,
+			leaf : false,
+			expanded : true
+		});
 
-        Genapp.tree.LayerTreePanel.superclass.initComponent.call(this);
-    },
+		// add plugins
+		this.plugins = this.plugins || [];
+		this.plugins.push({
+			ptype : "gx_treenodecomponent"
+		});
 
-    /**
-     * Add a loader to the node config if needed
-     * 
-     * @param {Ext.tree.TreeNode} nodeCfg The node config
-     * @hide
-     */ 
-    addLoaderToNodeConfig : function(nodeCfg){
-        if(!Ext.isEmpty(nodeCfg.nodeGroup)){
-            // Return the layers with the good nodeGroup
-            var filter = function(record) {
-                    var layerNodeGroup = record.get("layer").options.nodeGroup;
-                    if(!Ext.isEmpty(layerNodeGroup) && layerNodeGroup.indexOf(nodeCfg.nodeGroup) !== -1){
-                        return true;
-                    }
-                return false;
-            };
-            nodeCfg.loader = new GeoExt.tree.LayerLoader({
-                // Add the new filter
-                "filter" : filter,
-                // Override the default addLayerNode function to add the layer options
-                "addLayerNode" : function(node, layerRecord, index) {
-                    index = index || 0;
-                    if (this.filter(layerRecord) === true) {
-                        var child = this.createNode({
-                            nodeType: 'gx_layer',
-                            layer: layerRecord.getLayer(),
-                            layerStore: this.store,
-                            // New params
-                            checkedGroup: layerRecord.getLayer().options.checkedGroup,
-                            text: layerRecord.getLayer().options.label
-                        });
-                        var sibling = node.item(index);
-                        if(sibling) {
-                            node.insertBefore(child, sibling);
-                        } else {
-                            node.appendChild(child);
-                        }
-                        child.on("move", this.onChildMove, this);
-                    }
-                },
-                // Set the layersNodeIds object when the node order has changed
-                listeners:{
-                    "load" : this.setLayerNodeIds,
-                    scope: this
-                }
-            });
-        }
-    },
+		// Context menu with opacity slider, added by Lucia:
+		this.plugins.push(new Genapp.tree.ContextMenuPlugin({
+			sliderOptions : {
+				aggressive : true,
+				plugins : new GeoExt.LayerOpacitySliderTip()
+			}
+		})
 
-    /**
-     * Toggle the children checkbox on the parent checkbox change
-     * 
-     * @param {Ext.tree.TreeNode} node The parent node
-     * @param {Boolean} checked The checked status
-     * @hide
-     */
-    toggleChildrenOnParentToggle : function(node, checked) {
-        if (checked === true) {
-            for(var i = 0; i < node.childNodes.length; i++){
-                var child = node.childNodes[i];
-                if(!child.ui.isChecked()){
-                    child.ui.toggleCheck(true);
-                }
-            }
-        } else {
-            for(var i = 0; i < node.childNodes.length; i++){
-                var child = node.childNodes[i];
-                if(child.ui.isChecked()){
-                    child.ui.toggleCheck(false);
-                }
-            }
-        }
-    },
+		);
 
-    /**
-     * Set the layerNodeIds array
-     * 
-     * @hide
-     */
-    setLayerNodeIds : function(){
-        this.eachLayerChild(function(child){
-            this.layerNodeIds[child.layer.name] = child.id;
-        }, this);
-    },
+		// Toggle the children on the parent node 'checkchange' event
+		this.on('checkchange', this.toggleChildrenOnParentToggle, this);
 
-    /**
-     * Call the callback function for each layer child
-     * 
-     * @param {Function} fn The callback
-     * @param {Object} scope The scope for the callback
-     * @param {Array} args The arguments for the callback
-     * @param {Ext.tree.TreeNode} node The child parent node
-     */
-    eachLayerChild : function(fn, scope, args, node){
-        node = Ext.isEmpty(node) ? this.root : node;
-        if(!Ext.isEmpty(node) && !Ext.isEmpty(node.childNodes)){
-            for(var i = 0; i < node.childNodes.length; i++){
-                var child = node.childNodes[i];
-                if(!Ext.isEmpty(child.layer)){
-                    if(fn.apply(scope || child, args || [child]) === false){
-                        break;
-                    }
-                } else if(!child.isLeaf()) {
-                    this.eachLayerChild(fn, scope, args, child);
-                }
-            }
-        }
-    },
+		Genapp.tree.LayerTreePanel.superclass.initComponent.call(this);
+	},
 
-    /**
-     * Return the node for the passed layer name
-     * 
-     * @param {Ext.tree.TreeNode} layerName The layer name
-     */
-    getNodeByLayerName : function(layerName){
-        var nodeId = this.layerNodeIds[layerName];
-        if (Ext.isEmpty(nodeId)) {
-            this.setLayerNodeIds();
-        }
-        return this.getNodeById(nodeId);
-    },
+	/**
+	 * Add a loader to the node config if needed
+	 * 
+	 * @param {Ext.tree.TreeNode}
+	 *            nodeCfg The node config
+	 * @hide
+	 */
+	addLoaderToNodeConfig : function(nodeCfg) {
+		if (!Ext.isEmpty(nodeCfg.nodeGroup)) {
+			// Return the layers with the good nodeGroup
+			var filter = function(record) {
+				var layerNodeGroup = record.get("layer").options.nodeGroup;
+				if (!Ext.isEmpty(layerNodeGroup) && layerNodeGroup.indexOf(nodeCfg.nodeGroup) !== -1) {
+					return true;
+				}
+				return false;
+			};
+			nodeCfg.loader = new GeoExt.tree.LayerLoader({
+				// Add the new filter
+				"filter" : filter,
+				// Override the default addLayerNode function to add the layer
+				// options
+				"addLayerNode" : function(node, layerRecord, index) {
+					index = index || 0;
+					if (this.filter(layerRecord) === true) {
+						var child = this.createNode({
+							nodeType : 'gx_layer',
+							layer : layerRecord.getLayer(),
+							layerStore : this.store,
+							// New params
+							checkedGroup : layerRecord.getLayer().options.checkedGroup,
+							text : layerRecord.getLayer().options.label
+						});
 
-    /**
-     * Toggle the node checkbox
-     * 
-     * @param {Integer} nodeId The node id
-     * @param {Boolean} toggleCheck True to check, false to uncheck the box. 
-     * If no value was passed, toggles the checkbox
-     */
-    toggleNodeCheckbox : function(nodeId, toggleCheck){
-        var node = this.getNodeById(nodeId);
-        node.ui.toggleCheck(toggleCheck);
-        
-    }
+						var sibling = node.item(index);
+						if (sibling) {
+							node.insertBefore(child, sibling);
+						} else {
+							node.appendChild(child);
+						}
+					}
+				},
+				// Set the layersNodeIds object when the node order has changed
+				listeners : {
+					"load" : this.setLayerNodeIds,
+					scope : this
+				}
+			});
+		}
+	},
+
+	/**
+	 * Toggle the children checkbox on the parent checkbox change
+	 * 
+	 * @param {Ext.tree.TreeNode}
+	 *            node The parent node
+	 * @param {Boolean}
+	 *            checked The checked status
+	 * @hide
+	 */
+	toggleChildrenOnParentToggle : function(node, checked) {
+		if (checked === true) {
+			for ( var i = 0; i < node.childNodes.length; i++) {
+				var child = node.childNodes[i];
+				if (!child.ui.isChecked()) {
+					child.ui.toggleCheck(true);
+				}
+			}
+		} else {
+			for ( var i = 0; i < node.childNodes.length; i++) {
+				var child = node.childNodes[i];
+				if (child.ui.isChecked()) {
+					child.ui.toggleCheck(false);
+				}
+			}
+		}
+	},
+
+	/**
+	 * Set the layerNodeIds array
+	 * 
+	 * @hide
+	 */
+	setLayerNodeIds : function() {
+		this.eachLayerChild(function(child) {
+			this.layerNodeIds[child.layer.name] = child.id;
+		}, this);
+	},
+
+	/**
+	 * Display the contextual menu.
+	 */
+	displayContextualMenu : function() {
+		alert('toto : ');
+	},
+
+	/**
+	 * Call the callback function for each layer child
+	 * 
+	 * @param {Function}
+	 *            fn The callback
+	 * @param {Object}
+	 *            scope The scope for the callback
+	 * @param {Array}
+	 *            args The arguments for the callback
+	 * @param {Ext.tree.TreeNode}
+	 *            node The child parent node
+	 */
+	eachLayerChild : function(fn, scope, args, node) {
+		node = Ext.isEmpty(node) ? this.root : node;
+		if (!Ext.isEmpty(node) && !Ext.isEmpty(node.childNodes)) {
+			for ( var i = 0; i < node.childNodes.length; i++) {
+				var child = node.childNodes[i];
+				if (!Ext.isEmpty(child.layer)) {
+					if (fn.apply(scope || child, args || [ child ]) === false) {
+						break;
+					}
+				} else if (!child.isLeaf()) {
+					this.eachLayerChild(fn, scope, args, child);
+				}
+			}
+		}
+	},
+
+	/**
+	 * Return the node for the passed layer name
+	 * 
+	 * @param {Ext.tree.TreeNode}
+	 *            layerName The layer name
+	 */
+	getNodeByLayerName : function(layerName) {
+		var nodeId = this.layerNodeIds[layerName];
+		if (Ext.isEmpty(nodeId)) {
+			this.setLayerNodeIds();
+		}
+		return this.getNodeById(nodeId);
+	},
+
+	/**
+	 * Toggle the node checkbox
+	 * 
+	 * @param {Integer}
+	 *            nodeId The node id
+	 * @param {Boolean}
+	 *            toggleCheck True to check, false to uncheck the box. If no
+	 *            value was passed, toggles the checkbox
+	 */
+	toggleNodeCheckbox : function(nodeId, toggleCheck) {
+		var node = this.getNodeById(nodeId);
+		node.ui.toggleCheck(toggleCheck);
+
+	}
 });
 Ext.reg('layertreepanel', Genapp.tree.LayerTreePanel);
