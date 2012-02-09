2d4901174d2184b6eb591c1768b253e247d849b6
Benoit Pesty <Benoit.Pesty@ign.fr>
2012-02-09 14:15:06 +0000

    Recherche sur les caractères accentués

---
+++ Bases SQL/__Functions.sql
@@ -0,0 +1,18 @@
+
+/**
+ * This function is used to do accent-insensitive search.
+ */
+CREATE OR REPLACE FUNCTION unaccent_string(text) RETURNS text AS $$
+DECLARE
+    input_string text := $1;
+BEGIN
+
+input_string := translate(input_string, 'âãäåāăąÁÂÃÄÅĀĂĄ', 'aaaaaaaaaaaaaaa');
+input_string := translate(input_string, 'èééêëēĕėęěĒĔĖĘĚ', 'eeeeeeeeeeeeeee');
+input_string := translate(input_string, 'ìíîïìĩīĭÌÍÎÏÌĨĪĬ', 'iiiiiiiiiiiiiiii');
+input_string := translate(input_string, 'óôõöōŏőÒÓÔÕÖŌŎŐ', 'ooooooooooooooo');
+input_string := translate(input_string, 'ùúûüũūŭůÙÚÛÜŨŪŬŮ', 'uuuuuuuuuuuuuuuu');
+
+return input_string;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
