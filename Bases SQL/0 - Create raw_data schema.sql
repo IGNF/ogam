c5e0834c7d633a93d546e912935b3c2c7e6ac9db
Benoit Pesty <Benoit.Pesty@ign.fr>
2012-02-13 10:58:02 +0000

    Intégration des scripts dans le schéma qui va bien

--- Bases SQL/0 - Create raw_data schema.sql
+++ Bases SQL/0 - Create raw_data schema.sql
@@ -276,3 +276,23 @@ GRANT ALL ON TABLE raw_data.tree_data TO ogam;
 GRANT ALL ON TABLE raw_data.submission TO ogam;
 GRANT ALL ON TABLE raw_data.submission_file TO ogam;
 GRANT EXECUTE ON FUNCTION raw_data.geomfromcoordinate() TO ogam;
+
+
+
+/**
+ * This function is used to do accent-insensitive search.
+ */
+CREATE OR REPLACE FUNCTION unaccent_string(text) RETURNS text AS $$
+DECLARE
+    input_string text := $1;
+BEGIN
+
+input_string := translate(input_string, 'âãäåāăąÁÂÃÄÅĀĂĄ', 'aaaaaaaaaaaaaaa');
+input_string := translate(input_string, 'èééêëēĕėęěĒĔĖĘĚ', 'eeeeeeeeeeeeeee');
+input_string := translate(input_string, 'ìíîïìĩīĭÌÍÎÏÌĨĪĬ', 'iiiiiiiiiiiiiiii');
+input_string := translate(input_string, 'óôõöōŏőÒÓÔÕÖŌŎŐ', 'ooooooooooooooo');
+input_string := translate(input_string, 'ùúûüũūŭůÙÚÛÜŨŪŬŮ', 'uuuuuuuuuuuuuuuu');
+
+return input_string;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
--- Bases SQL/2 - Populate website schema.sql
+++ Bases SQL/2 - Populate website schema.sql
@@ -63,3 +63,46 @@ INSERT INTO permission_per_role(role_code, permission_code) VALUES ('ADMIN', 'CA
 INSERT INTO permission_per_role(role_code, permission_code) VALUES ('ADMIN', 'CANCEL_OTHER_PROVIDER_SUBMISSION');
 
 
+
+--
+-- Définition des requêtes prédéfinies
+--
+
+set search_path = website;
+
+DELETE FROM predefined_request_group_asso;
+DELETE FROM predefined_request_group;
+DELETE FROM predefined_request_criteria;
+DELETE FROM predefined_request_result;
+DELETE FROM predefined_request;
+
+-- Création d'un thème (groupe de requêtes)
+INSERT INTO predefined_request_group(group_name, label, definition, position) VALUES ('SPECIES', 'Distribution par espèce', 'Distribution par espèce', 1);
+
+-- Création d'une requête prédéfinie
+INSERT INTO predefined_request (request_name, schema_code, dataset_id, label, definition, date) VALUES ('SPECIES', 'RAW_DATA', 'SPECIES', 'Distribution par espèce', 'Distribution par espèce en forêt', now());
+INSERT INTO predefined_request (request_name, schema_code, dataset_id, label, definition, date) VALUES ('DEP', 'RAW_DATA', 'SPECIES', 'Espèces par département', 'Espèces par département', now());
+
+
+-- Configuration des requêtes prédéfinies
+INSERT INTO predefined_request_criteria (request_name, format, data, value, fixed) VALUES ('SPECIES', 'SPECIES_FORM', 'SPECIES_CODE', '026.001.006', NULL);
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('SPECIES', 'PLOT_FORM', 'PLOT_CODE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('SPECIES', 'PLOT_FORM', 'CYCLE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('SPECIES', 'PLOT_FORM', 'INV_DATE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('SPECIES', 'PLOT_FORM', 'IS_FOREST_PLOT');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('SPECIES', 'SPECIES_FORM', 'SPECIES_CODE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('SPECIES', 'SPECIES_FORM', 'BASAL_AREA');
+
+INSERT INTO predefined_request_criteria (request_name, format, data, value, fixed) VALUES ('DEP', 'LOCATION_FORM', 'DEPARTEMENT', '45', NULL);
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('DEP', 'PLOT_FORM', 'PLOT_CODE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('DEP', 'PLOT_FORM', 'CYCLE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('DEP', 'PLOT_FORM', 'INV_DATE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('DEP', 'PLOT_FORM', 'IS_FOREST_PLOT');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('DEP', 'SPECIES_FORM', 'SPECIES_CODE');
+INSERT INTO predefined_request_result (request_name, format, data) VALUES ('DEP', 'SPECIES_FORM', 'BASAL_AREA');
+
+
+-- Rattachement de la requête prédéfinies au thème
+INSERT INTO website.predefined_request_group_asso(group_name, request_name, position) VALUES ('SPECIES', 'SPECIES', 1);
+INSERT INTO website.predefined_request_group_asso(group_name, request_name, position) VALUES ('SPECIES', 'DEP', 2);
+
