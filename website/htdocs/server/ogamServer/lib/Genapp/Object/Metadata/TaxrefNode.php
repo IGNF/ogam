f6d3234d46d9e8c637c2690e2a4df14bcc7f2a71
Florent Bourcier <Florent.Bourcier@ign.fr>
2015-02-13 11:22:35 +0100

    fix : getnode in tree(ref) (should return the searched node not the tree child which contains the searched node)

--- website/htdocs/server/ogamServer/lib/Genapp/Object/Metadata/TaxrefNode.php
+++ website/htdocs/server/ogamServer/lib/Genapp/Object/Metadata/TaxrefNode.php
@@ -26,9 +26,8 @@ class Genapp_Object_Metadata_TaxrefNode extends Genapp_Object_Metadata_TreeNode 
 
 	/**
 	 * The childs.
-	 * @var Array[TaxrefNodes]
+	 * @var $children Array[TaxrefNodes]
 	 */
-	var $children = array();
 
 	/**
 	 * The short name of the taxon.
@@ -52,30 +51,8 @@ class Genapp_Object_Metadata_TaxrefNode extends Genapp_Object_Metadata_TreeNode 
 	 *
 	 * @param Genapp_Object_Metadata_TreeNode $child a node to add
 	 */
-	public function addChild($child) {
-		$this->children[] = $child;
-	}
 
-	/**
-	 * Return a node in the tree structure.
-	 *
-	 * @param String $code a node code
-	 * @return TreeNode the TreeNode found, null if not found
-	 */
-	public function getNode($aCode) {
 
-		if ($this->code == $aCode) {
-			return $this;
-		} else {
-			foreach ($this->children as $child) {
-				if ($child->getNode($aCode)) {
-					return $child;
-				}
-			}
-		}
-
-		return null;
-	}
 
 	/**
 	 * Serialize the object as a JSON.
