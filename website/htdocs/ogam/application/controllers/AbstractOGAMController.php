7047eec5f28e956d61f6d0afab6dde83433b262f
Benoit Pesty <Benoit.Pesty@ign.fr>
2015-05-27 12:10:43 +0000

    OGAM : Ménage

--- website/htdocs/ogam/application/controllers/AbstractOGAMController.php
+++ website/htdocs/ogam/application/controllers/AbstractOGAMController.php
@@ -14,7 +14,7 @@
 
 /**
  * AbstractOGAMController is the common controler for the OGAM application.
- * 
+ *
  * @package controllers
  */
 abstract class AbstractOGAMController extends Zend_Controller_Action {
@@ -54,57 +54,11 @@ abstract class AbstractOGAMController extends Zend_Controller_Action {
 		$userSession = new Zend_Session_Namespace('user');
 		$user = $userSession->user;
 		
-		// $this->logger->debug("preDispatch controller : " . $this->getRequest()->getControllerName());
-		// $this->logger->debug("preDispatch action : " . $this->getRequest()->getActionName());
-		
 		if (!empty($user)) {
 			$this->logger->debug("preDispatch user logged : " . $user->username);
 		} else {
 			$this->logger->debug('preDispatch user not logged (redirect to the user controller), session_id=' . session_id());
 			$this->_redirect('user');
 		}
-	}
-
-	/**
-	 * Return a list of the files contained in the provided directory and its subdirectories.
-	 *
-	 * @param string $dir
-	 *        	The directory
-	 * @param string $fileType
-	 *        	The file extension
-	 * @return Array[String] the list of files
-	 */
-	public static function getFilesInDir($dir, $fileType = null) {
-		$filesList = array();
-		$files = glob($dir . '/*');
-		foreach ($files as $file) {
-			if (is_dir($file)) {
-				$filesList = array_merge($filesList, AbstractOGAMController::getFilesInDir($file, $fileType));
-			} else {
-				if ($fileType == null) {
-					$filesList[] = $file;
-				} else if (substr($file, -3, 3) == $fileType) {
-					$filesList[] = $file;
-				}
-			}
-		}
-		return $filesList;
-	}
-
-	/**
-	 * Return a list of the files contained in the provided directories.
-	 *
-	 * @param array $dirs
-	 *        	The directories
-	 * @param string $fileType
-	 *        	The file extension
-	 * @return Array[String] the list of files
-	 */
-	public static function getFilesList($dirs, $fileType = null) {
-		$filesList = array();
-		foreach ($dirs as $filesDirectory) {
-			$filesList = array_merge($filesList, AbstractOGAMController::getFilesInDir($filesDirectory, $fileType));
-		}
-		return $filesList;
 	}
 }
