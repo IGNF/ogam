e926c0432749b7fa3cd7f51eebf76cb238c2836d
Benoit Pesty <Benoit.Pesty@ign.fr>
2015-07-22 14:16:35 +0200

    Maj script création tables

--- Bases SQL/0 - Create raw_data schema.sql
+++ Bases SQL/0 - Create raw_data schema.sql
@@ -321,27 +321,6 @@ COMMENT ON COLUMN CHECK_ERROR._CREATIONDT IS 'The creation date';
 
 
 
-/**
- * This function is used to do accent-insensitive search.
- */
-CREATE OR REPLACE FUNCTION unaccent_string(text) RETURNS text AS $$
-DECLARE
-    input_string text := $1;
-BEGIN
-
-input_string := translate(input_string, 'âãäåāăąÁÂÃÄÅĀĂĄ', 'aaaaaaaaaaaaaaa');
-input_string := translate(input_string, 'èééêëēĕėęěĒĔĖĘĚ', 'eeeeeeeeeeeeeee');
-input_string := translate(input_string, 'ìíîïìĩīĭÌÍÎÏÌĨĪĬ', 'iiiiiiiiiiiiiiii');
-input_string := translate(input_string, 'óôõöōŏőÒÓÔÕÖŌŎŐ', 'ooooooooooooooo');
-input_string := translate(input_string, 'ùúûüũūŭůÙÚÛÜŨŪŬŮ', 'uuuuuuuuuuuuuuuu');
-
-return input_string;
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
-
-
-
-
 
 GRANT ALL ON SCHEMA metadata TO ogam;
 GRANT ALL ON ALL TABLES IN SCHEMA raw_data TO ogam;
