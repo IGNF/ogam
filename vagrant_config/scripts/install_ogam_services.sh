68bd6757e4851b5ac486f77ab77b88ff315b3e2b
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-01-14 14:30:41 +0100

    Gradle : Config pour le service de génération de rapport
    Impossible de trouver une version correcte de BIRT sur un repository
    Maven. J'ai dû commiter l'archive WAR toute prête.
    
    Former-commit-id: cd6ebe7c86f878db672ba8cf5115c25077522a29

--- vagrant_config/scripts/install_ogam_services.sh
+++ vagrant_config/scripts/install_ogam_services.sh
@@ -18,5 +18,10 @@ sudo -n chown tomcat7:tomcat7 /var/tmp/ogam_upload
 
 
 service tomcat7 stop
-cd /vagrant/ogam/ && chmod a+x gradlew && bash gradlew && bash gradlew deploy -PtomcatHome='/var/lib/tomcat7' -PapplicationName='OGAM'
+
+cd /vagrant/ogam/ 
+chmod a+x gradlew 
+bash gradlew 
+bash gradlew deploy -PtomcatHome='/var/lib/tomcat7' -PapplicationName='OGAM'
+
 service tomcat7 start
