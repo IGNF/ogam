1c81fecf7a3f83de6761bcbc43eb1e0aaf146c84
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-01-26 14:08:39 +0100

    Erreur provisionnement
    Former-commit-id: af0fe18dfef4e91432cfb0e410b7fd97c176fd0e

--- vagrant_config/scripts/install_ogam_services.sh
+++ vagrant_config/scripts/install_ogam_services.sh
@@ -16,12 +16,6 @@ sudo -n chmod 774 /var/tmp/ogam_upload
 sudo -n chown tomcat7:tomcat7 /var/tmp/ogam_upload
 
 
-
 service tomcat7 stop
-
-cd /vagrant/ogam/ 
-chmod a+x gradlew 
-bash gradlew 
-bash gradlew deploy -PtomcatHome='/var/lib/tomcat7' -PapplicationName='OGAM'
-
+cd /vagrant/ogam/ && chmod a+x gradlew && bash gradlew && bash gradlew deploy -PtomcatHome='/var/lib/tomcat7' -PapplicationName='OGAM'
 service tomcat7 start
