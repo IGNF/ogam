1049ebe0a34b5af00898a104122950273bb615f7
Sylvain Galopin <Sylvain.Galopin@ign.fr>
2016-02-05 16:00:39 +0100

    OGAM-377 Refaire les boutons et le menu d'édition carto OGAM-378 Refaire les boutons de consultation carto
    Former-commit-id: 0ad3964a067bf3d5238d2c46d9fcc86a723e4624

--- website/htdocs/client/ogamDesktop/app/view/map/MapToolbar.js
+++ website/htdocs/client/ogamDesktop/app/view/map/MapToolbar.js
@@ -14,10 +14,165 @@ Ext.define('OgamDesktop.view.map.MapToolbar', {
     alias: 'widget.maptoolbar',
     xtype:'maptoolbar',
     items:[{
-        iconCls : 'o-map-tools-map-drawpoint',
-        tooltip: 'Add a point',
+        xtype:'buttongroup',
+        hidden: false,
+        action: 'drawing',
+        defaults: {
+          iconAlign:'top'
+        },
+        items:[{
+            iconCls : 'o-map-tools-map-zoomtodrawingfeatures',
+            tooltip: this.zoomToDrawingFeaturesButtonTooltip,
+            listeners: {
+                click: 'onZoomToDrawingFeaturesButtonPress'
+            }
+        },{ // Use of tbtext because tbseparator doesn't work...
+            xtype:'tbtext',
+            html: '|',
+            margin:'3 -3 0 -2',
+            style:'color:#aaa'
+        },{
+            xtype: 'splitbutton',
+            iconCls : 'o-map-tools-map-loadvectorlayer',
+            tooltip: this.loadVectorLayerButtonTooltip,
+            menu: [{
+                text:'Menu Item 1'
+            },{
+                text:'Menu Item 2'
+            },{
+                text:'Menu Item 3'
+            }]
+        },{
+            iconCls : 'o-map-tools-map-snapping',
+            tooltip: this.snappingButtonTooltip,
+            enableToggle: true,
+            listeners: {
+                click: 'onSnappingButtonPress'
+            }
+        },{ // Use of tbtext because tbseparator doesn't work...
+            xtype:'tbtext',
+            html: '|',
+            margin:'3 -3 0 -2',
+            style:'color:#aaa'
+        },{
+            iconCls : 'o-map-tools-map-drawpoint',
+            tooltip: this.drawPointButtonTooltip,
+            toggleGroup : "editing",
+            listeners: {
+                click: 'onDrawPointButtonPress'
+            }
+        },{
+            iconCls : 'o-map-tools-map-drawline',
+            tooltip: this.drawLineButtonTooltip,
+            toggleGroup : "editing",
+            listeners: {
+                click: 'onDrawLineButtonPress'
+            }
+        },{
+            iconCls : 'o-map-tools-map-drawpolygon',
+            tooltip: this.drawPolygonButtonTooltip,
+            toggleGroup : "editing",
+            listeners: {
+                click: 'onDrawPlygonButtonPress'
+            }
+        },{
+            iconCls : 'o-map-tools-map-selectFeature',
+            tooltip: this.selectFeatureButtonTooltip,
+            toggleGroup : "editing",
+            listeners: {
+                click: 'onSelectFeatureButtonPress'
+            }
+        },{ // Use of tbtext because tbseparator doesn't work...
+            xtype:'tbtext',
+            html: '|',
+            margin:'3 -3 0 -2',
+            style:'color:#aaa'
+        },{
+            iconCls : 'o-map-tools-map-modifyfeature',
+            tooltip: this.modifyFeatureButtonTooltip,
+            toggleGroup : "editing",
+            listeners: {
+                click: 'onModifyFeatureButtonPress'
+            }
+        },{
+            iconCls : 'o-map-tools-map-deletefeature',
+            tooltip: this.deleteFeatureButtonTooltip,
+            toggleGroup : "editing",
+            listeners: {
+                click: 'onDeleteFeatureButtonPress'
+            }
+        },{ // Use of tbtext because tbseparator doesn't work...
+            group : "drawValidation",
+            xtype:'tbtext',
+            html: '|',
+            margin:'3 -3 0 -2',
+            style:'color:#aaa'
+        },{
+            iconCls : 'o-map-tools-map-validateedition',
+            tooltip: this.validateEditionButtonTooltip,
+            listeners: {
+                click: 'onValidateEditionButtonPress'
+            }
+        },{
+            iconCls : 'o-map-tools-map-canceledition',
+            tooltip: this.cancelEditionButtonTooltip,
+            listeners: {
+                click: 'onCancelEditionButtonPress'
+            }
+        }]
+    },{
+        xtype : 'tbspacer',
+        flex: 1
+    },{
+        xtype: 'splitbutton',
+        iconCls : 'o-map-tools-map-layerfeatureinfo',
+        tooltip: this.layerFeatureInfoButtonTooltip,
+        toggleGroup : "consultation",
+        menu: [{
+            text:'Menu Item 1'
+        },{
+            text:'Menu Item 2'
+        },{
+            text:'Menu Item 3'
+        }]
+    },{
+        iconCls : 'o-map-tools-map-resultfeatureinfo',
+        tooltip: this.resultFeatureInfoButtonTooltip,
+        toggleGroup : "consultation",
         listeners: {
-            click: 'onDrawPointButtonPress'
+            click: 'onResultFeatureInfoButtonPress'
+        }
+    },'-',{
+        iconCls : 'o-map-tools-map-zoomin',
+        tooltip: this.zoomInButtonTooltip,
+        toggleGroup : "consultation",
+        listeners: {
+            click: 'onZoomInButtonPress'
+        }
+    },{
+        iconCls : 'o-map-tools-map-pan',
+        tooltip: this.mapPanButtonTooltip,
+        toggleGroup : "consultation",
+        listeners: {
+            click: 'onMapPanButtonPress'
+        }
+    },'-',{
+        iconCls : 'o-map-tools-map-zoomtoresultfeatures',
+        tooltip: this.zoomToResultFeaturesButtonTooltip,
+        listeners: {
+            click: 'onZoomToResultFeaturesButtonPress'
+        }
+    },{
+        iconCls : 'o-map-tools-map-zoomtomaxextent',
+        tooltip: this.zoomToMaxExtentButtonTooltip,
+        listeners: {
+            click: 'onZoomToMaxExtentButtonPress'
+        }
+    },'-',{
+        iconCls : 'o-map-tools-map-printMap',
+        tooltip: this.printMapButtonTooltip,
+        listeners: {
+            click: 'onPrintMapButtonPress'
         }
     }]
 });
