8fc657d73c1eaa628c1358ce190fc4ee2ea41aa4
florent bourcier <florent.bourcier@ign.fr>
2016-02-08 14:31:37 +0100

    OGAM-372 : formatter & position ol MousePosition controll
    Former-commit-id: e4c30f35ba5c86733f51fbf7169de6964f8242ea

--- website/htdocs/client/ogamDesktop/app/view/map/MapComponent.js
+++ website/htdocs/client/ogamDesktop/app/view/map/MapComponent.js
@@ -4,46 +4,53 @@ Ext.define("OgamDesktop.view.map.MapComponent",{
     xtype: 'mapcomponent',
     reference: 'mapCmp',
 
-    map: new ol.Map({
-        logo: false, // no attributions to ol
-        layers: [
-            new ol.layer.Vector({
-                code: 'drawingLayer',
-                name: 'Drawing layer',
-                source: new ol.source.Vector({features: new ol.Collection()}),
-                style: new ol.style.Style({
-                    fill: new ol.style.Fill({
-                        color: 'rgba(255, 255, 255, 0.2)'
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: '#ffcc33',
-                        width: 2
-                    }),
-                    image: new ol.style.Circle({
-                        radius: 7,
-                        fill: new ol.style.Fill({
-                          color: '#ffcc33'
-                        })
-                    })
-                })
-            })
-        ],
-        view: new ol.View({
-            resolutions: OgamDesktop.map.resolutions.slice(this.minZoomLevel),
-            projection : OgamDesktop.map.projection,
-            center: [OgamDesktop.map.x_center, OgamDesktop.map.y_center],
-            zoom: OgamDesktop.map.defaultzoom,
-            extent: [
-                OgamDesktop.map.x_min,
-                OgamDesktop.map.y_min,
-                OgamDesktop.map.x_max,
-                OgamDesktop.map.y_max
-            ]
-        }),
-        controls:  [
-            new ol.control.ZoomSlider(),
-            new ol.control.ScaleLine(),
-            new ol.control.MousePosition()
-        ]
-    })
-});
+	    map: new ol.Map({
+		logo: false, // no attributions to ol
+		layers: [
+		    new ol.layer.Vector({
+			code: 'drawingLayer',
+			name: 'Drawing layer',
+			source: new ol.source.Vector({features: new ol.Collection()}),
+			style: new ol.style.Style({
+			    fill: new ol.style.Fill({
+				color: 'rgba(255, 255, 255, 0.2)'
+			    }),
+			    stroke: new ol.style.Stroke({
+				color: '#ffcc33',
+				width: 2
+			    }),
+			    image: new ol.style.Circle({
+				radius: 7,
+				fill: new ol.style.Fill({
+				  color: '#ffcc33'
+				})
+			    })
+			})
+		    })
+		],
+		view: new ol.View({
+		    resolutions: OgamDesktop.map.resolutions.slice(this.minZoomLevel),
+		    projection : OgamDesktop.map.projection,
+		    center: [OgamDesktop.map.x_center, OgamDesktop.map.y_center],
+		    zoom: OgamDesktop.map.defaultzoom,
+		    extent: [
+			OgamDesktop.map.x_min,
+			OgamDesktop.map.y_min,
+			OgamDesktop.map.x_max,
+			OgamDesktop.map.y_max
+		    ]
+		}),
+		controls:  [
+		    new ol.control.ZoomSlider(),
+		    new ol.control.ScaleLine(),
+		    new ol.control.MousePosition({
+				className:'o-map-tools-map-mouse-position',
+				coordinateFormat :function(coords){
+					var template = 'x:{x} - Y:{y}';
+					return ol.coordinate.format(coords, template);
+		    }})
+		]
+	    })
+
+}
+);
