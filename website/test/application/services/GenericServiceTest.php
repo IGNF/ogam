6eefa28064da755f0c5851882a9ee3ad99cdfc28
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-02-11 14:48:52 +0100

    Ajout de tests unitaires
    Former-commit-id: 451f5990779a35e8127fb6844b980b0e98a1f008

--- website/test/application/services/GenericServiceTest.php
+++ website/test/application/services/GenericServiceTest.php
@@ -141,10 +141,110 @@ class GenericServiceTest extends ControllerTestCase {
 	 * Test de la fonction removeAccents().
 	 */
 	public function testRemoveAccents() {
-
 		$label = $this->queryService->removeAccents('Tèst d\'élimînàtiôn des âçcènts');
 
 		$this->assertEquals('Test d\'elimination des accents', $label);
+	}
 
+	/**
+	 * Test de la fonction buildDataObject().
+	 */
+	public function testBuildDataObject() {
+
+		// On récupère un descripteur d'objet pour le format "Location"
+		// On ne précise pas de dataset, il n'y a donc pas de filtrage sur les champs disponibles
+		$data = $this->queryService->buildDataObject('RAW_DATA', 'LOCATION_DATA');
+
+		// $this->logger->info('data : ' . print_r($data, true));
+
+		// Le descripteur n'est pas null
+		$this->assertNotNull($data);
+
+		// Il correspond à un format LOCATION
+		$this->assertNotNull($data->tableFormat);
+		$this->assertEquals('LOCATION_DATA', $data->tableFormat->format);
+		$this->assertEquals('LOCATION', $data->tableFormat->tableName);
+
+		// Il contient 2 champs de clé primaire
+		$this->assertNotNull($data->getInfoFields());
+		$this->assertTrue(is_array($data->getInfoFields()));
+		$this->assertEquals(2, count($data->getInfoFields()));
+
+		$providerField = $data->getInfoField('LOCATION_DATA__PROVIDER_ID');
+		$plotField = $data->getInfoField('LOCATION_DATA__PLOT_CODE');
+		$this->assertEquals('PROVIDER_ID', $providerField->data);
+		$this->assertEquals('PLOT_CODE', $plotField->data);
+
+		// Et des champs de données
+		$this->assertNotNull($data->getEditableFields());
+		$this->assertTrue(is_array($data->getEditableFields()));
+		$this->assertEquals(8, count($data->getEditableFields()));
+
+		$latitudeField = $data->getEditableField('LOCATION_DATA__LATITUDE');
+		$this->assertEquals('LATITUDE', $latitudeField->data);
+
+		// Les formats utilisés
+		$this->assertNotNull($data->getFormats());
+		$this->assertTrue(is_array($data->getFormats()));
+		$this->assertTrue(in_array('LOCATION_DATA', $data->getFormats()));
+
+		// Le format LOCALISATION possède une géométrie
+		$this->assertTrue($data->hasGeometry());
+
+		// On renseigne des valeurs pour les éléments de la clé
+		$providerField->value = 'TestProvider';
+		$plotField->value = '1234';
+
+		// On vérifie l'identificant unique de la donnée
+		$this->assertEquals('SCHEMA/RAW_DATA/FORMAT/LOCATION_DATA/PROVIDER_ID/TestProvider/PLOT_CODE/1234', $data->getId());
+	}
+
+	/**
+	 * Test de la fonction buildDataObject() dans le cas où on précise un dataset.
+	 */
+	public function testBuildDataObjectWithDataset() {
+
+		// On récupère un descripteur d'objet pour le format "Location"
+		// On ne précise pas de dataset
+		$data = $this->queryService->buildDataObject('RAW_DATA', 'LOCATION_DATA', 'SPECIES');
+
+		// $this->logger->info('data : ' . print_r($data, true));
+
+		// Le descripteur n'est pas null
+		$this->assertNotNull($data);
+
+		// Il correspond à un format LOCATION
+		$this->assertNotNull($data->tableFormat);
+		$this->assertEquals('LOCATION_DATA', $data->tableFormat->format);
+		$this->assertEquals('LOCATION', $data->tableFormat->tableName);
+
+		// Et des champs de données
+		$this->assertNotNull($data->getEditableFields());
+		$this->assertTrue(is_array($data->getEditableFields()));
+		$this->assertEquals(8, count($data->getEditableFields()));
+	}
+
+	/**
+	 * Test de la fonction getAllFormats().
+	 */
+	public function testGetAllFormats() {
+
+		// On récupère un descripteur d'objet pour le format "SPECIES"
+		$data = $this->queryService->buildDataObject('RAW_DATA', 'SPECIES_DATA', 'SPECIES');
+
+		// On récupère la liste des formats de tables dans la hiérarchie de cet objet.
+		$hierarchie = $this->queryService->getAllFormats('RAW_DATA', $data);
+
+		// $this->logger->info('hierarchie : ' . print_r($hierarchie, true));
+
+		$this->assertNotNull($hierarchie);
+		$this->assertTrue(is_array($hierarchie));
+		$this->assertEquals(3, count($hierarchie));
+
+		// Les 3 tables dans l'ordre doivent être LOCATION, PLOT, SPECIES
+		$keys = array_keys($hierarchie);
+		$this->assertEquals('LOCATION_DATA', $keys[0]);
+		$this->assertEquals('PLOT_DATA', $keys[1]);
+		$this->assertEquals('SPECIES_DATA', $keys[2]);
 	}
 }
