f2961fe0de4121395ea10e55e3a13255a61dcb37
Gautam <gautam.pastakia@ign.fr>
2016-03-22 15:09:12 +0100

    Formatted setup.php.
    Former-commit-id: 05735eaaef97630e002540bc8915fd04a80f55c2

--- website/htdocs/ogam/public/setup.php
+++ website/htdocs/ogam/public/setup.php
@@ -1,7 +1,7 @@
 <?php
 
 // Define path to application directory
-defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../application'));
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 // Define application environment
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
@@ -10,18 +10,18 @@ defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV
 define('DATE_STAMP', date('Y-m-d'));
 
 // Define path to ogam custom application directory
-if (file_exists(APPLICATION_PATH.'/../../custom')) {
-	define('CUSTOM_APPLICATION_PATH', APPLICATION_PATH.'/../../custom/application');
+if (file_exists(APPLICATION_PATH . '/../../custom')) {
+	define('CUSTOM_APPLICATION_PATH', APPLICATION_PATH . '/../../custom/application');
 }
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-	realpath(APPLICATION_PATH.'/../library'),
+	realpath(APPLICATION_PATH . '/../library'),
 	get_include_path()
 )));
-if (defined('CUSTOM_APPLICATION_PATH') && file_exists(CUSTOM_APPLICATION_PATH.'/../library')) {
+if (defined('CUSTOM_APPLICATION_PATH') && file_exists(CUSTOM_APPLICATION_PATH . '/../library')) {
 	set_include_path(implode(PATH_SEPARATOR, array(
-		realpath(CUSTOM_APPLICATION_PATH.'/../library'),
+		realpath(CUSTOM_APPLICATION_PATH . '/../library'),
 		get_include_path()
 	)));
 }
@@ -29,9 +29,10 @@ if (defined('CUSTOM_APPLICATION_PATH') && file_exists(CUSTOM_APPLICATION_PATH.'/
 require_once 'Zend/Config/Ini.php';
 
 // Create application, bootstrap, and run
-$applicationIniFilePath = APPLICATION_PATH.'/configs/application.ini';
-if (defined('CUSTOM_APPLICATION_PATH') && file_exists(CUSTOM_APPLICATION_PATH.'/configs/application.ini')) {
-	$applicationIniFilePath = CUSTOM_APPLICATION_PATH.'/configs/application.ini';
+$applicationIniFilePath = APPLICATION_PATH . '/configs/application.ini';
+if (defined('CUSTOM_APPLICATION_PATH') && file_exists(CUSTOM_APPLICATION_PATH . '/configs/application.ini')) {
+	$applicationIniFilePath = CUSTOM_APPLICATION_PATH . '/configs/application.ini';
 }
-$ApplicationConf = new Zend_Config_Ini($applicationIniFilePath, APPLICATION_ENV, array('allowModifications' => true));
-
+$ApplicationConf = new Zend_Config_Ini($applicationIniFilePath, APPLICATION_ENV, array(
+	'allowModifications' => true
+));
