8258c7d1dd98bb2b0e379b660320f77a9630f948
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-03-23 12:02:20 +0100

    Début de remise en place de PHPUnit
    Former-commit-id: d738db1afade1c2cf1be52f7ac5f089f005af2f3

--- website/htdocs/server/test/app/Bootstrap.php
+++ website/htdocs/server/test/app/Bootstrap.php
@@ -7,16 +7,16 @@
 error_reporting(E_ALL | E_STRICT);
 
 // Redefine the paths to link to the correct directories
-define('APPLICATION_PATH', './htdocs/ogam/application/');
-define('LIBRARY_PATH', './htdocs/ogam/library/');
+define('APPLICATION_PATH', './ogamServer/app/');
+define('LIBRARY_PATH', './ogamServer/lib/');
 define('TEST_PATH', './test/');
 define('APPLICATION_ENVIRONMENT', 'production');
-define('APPLICATION_LOG_PATH', './htdocs/logs');
+define('APPLICATION_LOG_PATH', './logs');
 
 // ------------------------------------------ //
 // Include the original application bootstrap //
 // ------------------------------------------ //
-include APPLICATION_PATH . '/../public/setup.php';
+include APPLICATION_PATH . '../../../public/setup.php';
 
 require_once 'Zend/Application.php';
 
