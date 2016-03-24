32351f006536a5e08bd51de2b6733b9e3f39d569
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-03-24 15:59:49 +0100

    pb de chemin
    Former-commit-id: 872019906d24790b3be01723e1b8229530accc69

--- vagrant_config/scripts/build_ogam_desktop.sh
+++ vagrant_config/scripts/build_ogam_desktop.sh
@@ -2,4 +2,4 @@
 # Launch Sencha Cmd to compile the JS and CSS
 #----------------------------------------------------------------
 cd /vagrant/ogam/website/htdocs/client/ogamDesktop
-/root/bin/Sencha/Cmd/sencha app build
+/vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/sencha app build
