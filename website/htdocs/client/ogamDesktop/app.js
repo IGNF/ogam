57ea557145570f2e929bc9e89cbf9b985db37884
Sylvain Galopin <Sylvain.Galopin@ign.fr>
2016-04-04 17:43:27 +0200

    Mise à jour de app.js (autoCreateViewport est déprécié)
    Former-commit-id: 3f6e96bf3b7941c1cfe3604c8f4a617b736ca1ee

--- website/htdocs/client/ogamDesktop/app.js
+++ website/htdocs/client/ogamDesktop/app.js
@@ -8,7 +8,15 @@ Ext.application({
 
     extend: 'OgamDesktop.Application',
     
-    autoCreateViewport: 'OgamDesktop.view.main.Main'
+    requires: [
+        'OgamDesktop.view.main.Main'
+    ],
+
+    // The name of the initial view to create. With the classic toolkit this class
+    // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
+    // modern toolkit, the main view will be added to the Viewport.
+    //
+    mainView: 'OgamDesktop.view.main.Main'
 	
     //-------------------------------------------------------------------------
     // Most customizations should be made to OgamDesktop.Application. If you need to
