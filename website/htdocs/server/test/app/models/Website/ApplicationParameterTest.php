b6ed4c9fcc696aa68f46047df22a69c68e106611
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-05-17 09:01:56 +0200

    Maj test unitaire
    Former-commit-id: 6cb815e744dc55805c3e1a9b1c14c12426fb99b0

--- website/htdocs/server/test/app/models/Website/ApplicationParameterTest.php
+++ website/htdocs/server/test/app/models/Website/ApplicationParameterTest.php
@@ -12,15 +12,19 @@ class ApplicationParameterTest extends ControllerTestCase {
 	 * Test de la fonction getParameters().
 	 */
 	public function testGetParameters() {
-		
+
 		// On charge le modèle
 		$parametersModel = new Application_Model_Website_ApplicationParameter();
-		
+
 		// On récupère les paramètres
 		$params = $parametersModel->getParameters();
-		
+
 		// On vérifie que l'on a ramené les bonnes valeurs
 		$this->assertNotNull($params);
-		$this->assertEquals('OK', $params->Test);
+		$this->assertTrue(is_array($params));
+
+		$testParam = $params['Test'];
+
+		$this->assertEquals('OK', $testParam->value);
 	}
 }
