a12684a062b487ffbf2d97fa30287fb9430a5a9f
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-07-27 08:32:16 +0200

    Ajout entités System
    Former-commit-id: 8832a865345d997e66d38c6de8efc4902ad70b5b

--- database/Update Scripts/Migration Symfony.sql
+++ database/Update Scripts/Migration Symfony.sql
@@ -1,16 +1,23 @@
 
 --
 -- On liste ici les modifications effectuées sur la base de donnée suite à la migration symfony 
--- (passage de char(1) à boolean, etc).
-
 
 --Il y avait des incohérences entre les entités (les objets) et la base de données (colonnes non mappées ou attributs non sauvegardés).
 --Il faudra essayer de les corriger.
-
---Le champ "position" dans les fields et les formats pourrait être remonté au niveau de la classe parente.
 
 -- Ajout du champ "position" dans la table "mode_taxref"
 ALTER TABLE metadata.mode_taxref ADD column "position" integer;
 
 -- Renommage de "name" en "label" dans la table "mode_taxref"
 ALTER TABLE metadata.mode_taxref RENAME name  TO label;
+
+
+-- TODO : Le champ "position" dans les fields et les formats pourrait être remonté au niveau de la classe parente.
+
+-- TODO : Passage de char(1) à boolean pour les attributs booléens.
+
+
+-- TODO :
+-- On avait des objets "TreeNode" et "TaxrefNode" qui représentairent un noeud d'un arbre avec ses enfants (attribut children).
+-- Alors qu'en base on a la vision inverse, on a une entité "ModeTree" avec un lien vers son parent.
+-- La conversion était faite dans le modèle par un appel récursif de fonction.
