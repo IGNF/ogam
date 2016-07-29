5b4df85934d6e08c13bb70b42079191377b6f9df
Sylvain Galopin <Sylvain.Galopin@ign.fr>
2016-07-29 17:17:51 +0200

    OGAM-734 Tester et mettre en place la nouvelle version de sencha cmd (6.0.2.14 -> 6.1.3.42)
    Former-commit-id: 72bab9db06d1143793a88dbf298628a92c1294a1

--- vagrant_config/scripts/install_sencha_cmd_6.sh
+++ vagrant_config/scripts/install_sencha_cmd_6.sh
@@ -23,23 +23,23 @@ sudo -n apt-get install -y unzip
 #export https_proxy=proxy.ign.fr:3128
 #export http_proxy=proxy.ign.fr:3128
 
-if [ ! -f "./SenchaCmd-6.0.2.14-linux-amd64.sh.zip" ]; then
-	wget http://cdn.sencha.com/cmd/6.0.2.14/no-jre/SenchaCmd-6.0.2.14-linux-amd64.sh.zip 2>&1
-	unzip ./SenchaCmd-6.0.2.14-linux-amd64.sh.zip
+if [ ! -f "./SenchaCmd-6.1.3-linux-amd64.sh.zip" ]; then
+	wget http://cdn.sencha.com/cmd/6.1.3/no-jre/SenchaCmd-6.1.3-linux-amd64.sh.zip 2>&1
+	unzip ./SenchaCmd-6.1.3-linux-amd64.sh.zip
 fi
 
 #----------------------------------------------------------------
 # Install Sencha Cmd
 # http://resources.ej-technologies.com/install4j/help/doc/index.html
 #----------------------------------------------------------------
-sudo mkdir -p /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.0.2.14/
+sudo mkdir -p /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.1.3.42/
 sudo chown -R www-data:www-data /vagrant/ogam/website/htdocs/client/vendor
 sudo chmod -R 774 /vagrant/ogam/website/htdocs/client/vendor
-sudo ./SenchaCmd-6.0.2.14-linux-amd64.sh -q -dir /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.0.2.14/
+sudo ./SenchaCmd-6.1.3.42-linux-amd64.sh -q -dir /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.1.3.42/
 
 # Ajout du proxy IGN à la config 
-#echo '-Dhttp.proxyHost=proxy.ign.fr' >> /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.0.2.14/sencha.vmoptions
-#echo '-Dhttp.proxyPort=3128' >> /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.0.2.14/sencha.vmoptions
+#echo '-Dhttp.proxyHost=proxy.ign.fr' >> /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.1.3.42/sencha.vmoptions
+#echo '-Dhttp.proxyPort=3128' >> /vagrant/ogam/website/htdocs/client/vendor/Sencha/Cmd/6.1.3.42/sencha.vmoptions
 
 
 #----------------------------------------------------------------
