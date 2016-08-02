8837db5bd370aa5dcaba5e87d4c3eacb57d71d86
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-08-02 16:06:56 +0200

    Debut de mise en place d'authentification
    Former-commit-id: 30012ac8e27c9dc677864149cb8c95939c572d78

--- website/htdocs/server/ogamServer/src/OGAMBundle/Controller/DefaultController.php
+++ website/htdocs/server/ogamServer/src/OGAMBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace OGAMBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller {
 
