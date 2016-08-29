04baaf31a37551dc45e18da97e1503552d64fb91
florent bourcier <florent.bourcier@ign.fr>
2016-08-29 19:39:10 +0200

    refact, deadcode
    Former-commit-id: e8054cf33f588bdf21177d9f7ba0c8003bc6b880

--- website/htdocs/server/ogamServer/src/OGAMBundle/Controller/HarmonizationController.php
+++ website/htdocs/server/ogamServer/src/OGAMBundle/Controller/HarmonizationController.php
@@ -29,7 +29,7 @@ class HarmonizationController extends Controller
     	$activeSubmissions = $this->getDoctrine()->getRepository('OGAMBundle:RawData\Submission','raw_data')->getSubmissionsForHarmonization();
     	$HarmoRepo = $this->getDoctrine()->getRepository('OGAMBundle:HarmonizedData\HarmonizationProcess','harmonized_data');
     	$harmonisationProcesses = array();
-    	
+
     	foreach ($activeSubmissions as $activeSubmission) {
     		$harmonisationProcess = $HarmoRepo->findOneBy(array('providerId'=>$activeSubmission->getProvider()->getId(), 'dataset'=>$activeSubmission->getDataset()), array('harmonizationId'=> 'DESC'));
     		if (empty($harmonisationProcess)) {
@@ -38,47 +38,7 @@ class HarmonizationController extends Controller
     		}
     		$harmonisationProcesses[] = $harmonisationProcess;
     	}
-    	
-    	/*
-    	// Get the list of available harmonization (active submissions)
-    	$activeSubmissions = $this->submissionModel->getSubmissionsForHarmonization();
-    	
-    	$harmonisationProcesses = array();
-    	
-    	foreach ($activeSubmissions as $id => $activeSubmission) {
-    	
-    		// Get the status of the last process run
-    		$process = $this->harmonizationModel->getHarmonizationProcessInfo($activeSubmission);
-    	
-    		// Get the source submissions of this process
-    		$process = $this->harmonizationModel->getHarmonizationProcessSources($process);
-    	
-    		// Get the current status of the source data
-    		if($process->status === 'UNDONE'){
-    			$submission = $this->submissionModel->getSubmission($activeSubmission->submissionId);
-    			if ($submission->step === "VALIDATED") {
-    				$submissionStatus = "VALIDATED";
-    			} else {
-    				$submissionStatus = "NOT_VALID";
-    			}
-    		} else {
-    			$submissionStatus = 'VALIDATED';
-    			foreach ($process->submissionIDs as $submissionID) {
-    				$submission = $this->submissionModel->getSubmission($submissionID);
-    				if ($submission->step !== "VALIDATED") {
-    					$submissionStatus = "NOT_VALID";
-    				}
-    			}
-    		}
-    	
-    		$process->submissionStatus = $submissionStatus;
-    	
-    		$harmonisationProcesses[$id] = $process;
-    	}
-    	
-    	// Send the data to the view
-    	$this->view->harmonizations = $harmonisationProcesses;
-    	*/
+
         return $this->render('OGAMBundle:Harmonization:show_harmonization_page.html.twig', array(
             'harmonizations' => $harmonisationProcesses
         ));
