e8b9876678d5e8d73696ab89c4fd0d7b5bf7a5ca
florent bourcier <florent.bourcier@ign.fr>
2016-08-30 14:28:27 +0200

    Mapcontroller:GetMapParameters
    Former-commit-id: 91a36c3270dd45ad0b1cdc34cbd438a04152fb6e

--- website/htdocs/server/ogamServer/src/OGAMBundle/Controller/MapController.php
+++ website/htdocs/server/ogamServer/src/OGAMBundle/Controller/MapController.php
@@ -5,6 +5,7 @@ namespace OGAMBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 /**
  * 
  * @Route("/map")
@@ -24,6 +25,7 @@ class MapController extends Controller
 		// Get the parameters from configuration file
 		$configuration = $this->get("ogam.configuration_manager");
 		$view = new \ArrayObject();
+		$view->setFlags(\ArrayObject::ARRAY_AS_PROPS);
 		$view->bbox_x_min = $configuration->getConfig('bbox_x_min'); // x min of Bounding box
 		$view->bbox_y_min = $configuration->getConfig('bbox_y_min'); // y min of Bounding box
 		$view->bbox_x_max = $configuration->getConfig('bbox_x_max'); // x max of Bounding box
@@ -32,7 +34,7 @@ class MapController extends Controller
 		$view->projection = "EPSG:" . $configuration->getConfig('srs_visualisation'); // Projection
 
 		// Get the available scales
-		$scales = $this->scalesModel->getScales();
+		$scales = $this->get('ogam.repository.mapping.scale')->getScales();
 
 		// Transform the available scales into resolutions
 		$resolutions = $this->getResolutions($scales);
@@ -44,15 +46,15 @@ class MapController extends Controller
 
 		if ($userPerProviderCenter) {
 			// Center the map on the provider location
-			$center = $this->boundingBoxModel->getCenter($providerId);
+			$center = $this->getDoctrine()->getEntityManagerForClass('OGAMBundle\Entity\Mapping\BoundingBox')->find('OGAMBundle\Entity\Mapping\BoundingBox', $providerId)->getCenter();
 			$view->zoomLevel = $center->zoomLevel;
 			$view->centerX = $center->x;
 			$view->centerY = $center->y;
 		} else {
 			// Use default settings
 			$view->zoomLevel = $configuration->getConfig('zoom_level', '1');
-			$view->centerX = ($this->view->bbox_x_min + $this->view->bbox_x_max) / 2;
-			$view->centerY = ($this->view->bbox_y_min + $this->view->bbox_y_max) / 2;
+			$view->centerX = ($view->bbox_x_min + $view->bbox_x_max) / 2;
+			$view->centerY = ($view->bbox_y_min + $view->bbox_y_max) / 2;
 		}
 
 		// Feature parameters
@@ -60,7 +62,34 @@ class MapController extends Controller
 		$view->featureinfo_typename = $configuration->getConfig('featureinfo_typename', "result_locations");
 		$view->featureinfo_maxfeatures = $configuration->getConfig('featureinfo_maxfeatures', 20);
 
-        return $this->render('OGAMBundle:Map:get_map_parameters.js.twig', $view)->headers->set('Content-type', 'application/javascript');
+        return $this->render('OGAMBundle:Map:get_map_parameters.js.twig', $view->getArrayCopy(), (new Response())->headers->set('Content-type', 'application/javascript'));
+    }
+    
+    /**
+     * Calculate the resolution array corresponding to the available scales.
+     *
+     * Not private because can be used by custom controllers.
+     *
+     * @param Array[Integer] $scales
+     *        	The list of scales
+     * @return Array[Integer] the resolutions
+     */
+    protected function getResolutions($scales) {
+    
+    	// Get the parameters from configuration file
+    	$configuration = $this->get("ogam.configuration_manager");
+    	$tilesize = $configuration->getConfig('tilesize', 256); // Tile size in pixels
+    	$dpi = $configuration->getConfig('mapserver_dpi', 72); // Default number of dots per inch in mapserv
+    	$factor = $configuration->getConfig('mapserver_inch_per_kilometer', 39370.1); // Inch to meter conversion factor
+    
+    	// WARNING : Bounding box must match the tilecache configuration and tile size
+    
+    	$resolutions = array();
+    	foreach ($scales as $scale) {
+    		$res = $scale * (2 * $tilesize) / ($dpi * $factor);
+    		$resolutions[$scale] = $res / (2 * $tilesize) * 1000;
+    	}
+    	return $resolutions;
     }
 
     /**
