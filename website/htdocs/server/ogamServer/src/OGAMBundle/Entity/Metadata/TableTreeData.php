45f67fe2a78bfeb6ef1bb701fc26cb918b4bf6af
Benoit Pesty <Benoit.Pesty@ign.fr>
2016-09-02 12:16:43 +0200

    Modif mapping entité TableTree
    Former-commit-id: 34731e56f35d377fe9f5cae0d5aba5076f47c418

--- website/htdocs/server/ogamServer/src/OGAMBundle/Entity/Metadata/TableTreeData.php
+++ website/htdocs/server/ogamServer/src/OGAMBundle/Entity/Metadata/TableTreeData.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace OGAMBundle\Entity\Metadata;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -10,137 +9,132 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="metadata.table_tree")
  * @ORM\Entity(repositoryClass="OGAMBundle\Repository\Metadata\tableTreeDataRepository")
  */
-class TableTreeData
-{
-    /**
-     * @ORM\ManyToOne(targetEntity="TableFormat")
-     * @ORM\JoinColumn(name="child_table", referencedColumnName="format")
-     * @ORM\Id
-     */
-    private $tableFormat;
+class TableTreeData {
 
-    /**
-     * @ORM\ManyToOne(targetEntity="TableFormat")
-     * @ORM\JoinColumn(name="parent_table", referencedColumnName="format")
+	/**
+	 * @ORM\ManyToOne(targetEntity="Schema")
+	 * @ORM\JoinColumn(name="schema_code", referencedColumnName="schema_code")
 	 * @ORM\Id
-     */
-    private $parentTableFormat;
+	 */
+	private $schema;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="join_key", type="string", length=255)
-     */
-    private $joinKeys;
+	/**
+	 * @ORM\ManyToOne(targetEntity="TableFormat")
+	 * @ORM\JoinColumn(name="child_table", referencedColumnName="format")
+	 * @ORM\Id
+	 */
+	private $tableFormat;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="comment", type="string", length=255)
-     */
-    private $comment;
+	/**
+	 * @ORM\ManyToOne(targetEntity="TableFormat")
+	 * @ORM\JoinColumn(name="parent_table", referencedColumnName="format")
+	 */
+	private $parentTableFormat;
 
+	/**
+	 *
+	 * @var string @ORM\Column(name="join_key", type="string", length=255)
+	 */
+	private $joinKeys;
 
-    /**
-     * Set tableFormat
-     *
-     * @param string $tableFormat
-     *
-     * @return tableTreeData
-     */
-    public function setTableFormat($tableFormat)
-    {
-        $this->tableFormat = $tableFormat;
+	/**
+	 *
+	 * @var string @ORM\Column(name="comment", type="string", length=255)
+	 */
+	private $comment;
 
-        return $this;
-    }
+	/**
+	 * Set tableFormat
+	 *
+	 * @param string $tableFormat
+	 *
+	 * @return tableTreeData
+	 */
+	public function setTableFormat($tableFormat) {
+		$this->tableFormat = $tableFormat;
 
-    /**
-     * Get tableFormat
-     *
-     * @return string
-     */
-    public function getTableFormat()
-    {
-        return $this->tableFormat;
-    }
+		return $this;
+	}
 
-    /**
-     * Set parentTableFormat
-     *
-     * @param string $parentTableFormat
-     *
-     * @return tableTreeData
-     */
-    public function setParentTableFormat($parentTableFormat)
-    {
-        $this->parentTableFormat = $parentTableFormat;
+	/**
+	 * Get tableFormat
+	 *
+	 * @return string
+	 */
+	public function getTableFormat() {
+		return $this->tableFormat;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set parentTableFormat
+	 *
+	 * @param string $parentTableFormat
+	 *
+	 * @return tableTreeData
+	 */
+	public function setParentTableFormat($parentTableFormat) {
+		$this->parentTableFormat = $parentTableFormat;
 
-    /**
-     * Get parentTableFormat
-     *
-     * @return string
-     */
-    public function getParentTableFormat()
-    {
-        return $this->parentTableFormat;
-    }
+		return $this;
+	}
 
-    /**
-     * Set joinKeys
-     *
-     * @param string $joinKeys
-     *
-     * @return tableTreeData
-     */
-    public function setJoinKeys($joinKeys)
-    {
-        $this->joinKeys = implode(",", $joinKeys);
+	/**
+	 * Get parentTableFormat
+	 *
+	 * @return string
+	 */
+	public function getParentTableFormat() {
+		return $this->parentTableFormat;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set joinKeys
+	 *
+	 * @param string $joinKeys
+	 *
+	 * @return tableTreeData
+	 */
+	public function setJoinKeys($joinKeys) {
+		$this->joinKeys = implode(",", $joinKeys);
 
-    /**
-     * Get joinKeys
-     *
-     * @return string
-     */
-    public function getJoinKeys()
-    {
-        $joinKeys = array();
-    	$pks = explode(",", $this->joinKeys);
-    	foreach ($pks as $pk) {
-    		$joinKeys[] = trim($pk); // we need to trim all the values
-    	}
+		return $this;
+	}
 
-        return $joinKeys;
-    }
+	/**
+	 * Get joinKeys
+	 *
+	 * @return string
+	 */
+	public function getJoinKeys() {
+		$joinKeys = array();
+		$pks = explode(",", $this->joinKeys);
+		foreach ($pks as $pk) {
+			$joinKeys[] = trim($pk); // we need to trim all the values
+		}
 
-    /**
-     * Set comment
-     *
-     * @param string $comment
-     *
-     * @return tableTreeData
-     */
-    public function setComment($comment)
-    {
-        $this->comment = $comment;
+		return $joinKeys;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set comment
+	 *
+	 * @param string $comment
+	 *
+	 * @return tableTreeData
+	 */
+	public function setComment($comment) {
+		$this->comment = $comment;
 
-    /**
-     * Get comment
-     *
-     * @return string
-     */
-    public function getComment()
-    {
-        return $this->comment;
-    }
+		return $this;
+	}
+
+	/**
+	 * Get comment
+	 *
+	 * @return string
+	 */
+	public function getComment() {
+		return $this->comment;
+	}
 }
 
