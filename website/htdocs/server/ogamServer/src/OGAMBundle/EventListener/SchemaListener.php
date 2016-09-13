35cd83b4e78566946d63adc94331066143ca2e46
Sylvain Galopin <Sylvain.Galopin@ign.fr>
2016-09-13 14:59:42 +0200

    SchemaListener
    Former-commit-id: 78bbfb56df88ce7c4eef68dff66641c3631c25a9

--- website/htdocs/server/ogamServer/src/OGAMBundle/EventListener/SchemaListener.php
+++ website/htdocs/server/ogamServer/src/OGAMBundle/EventListener/SchemaListener.php
@@ -5,46 +5,53 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class SchemaListener implements EventSubscriberInterface {
+class SchemaListener implements EventSubscriberInterface
+{
 
-	private $defaultSchema;
+    private $defaultSchema;
 
-	protected $schema;
+    protected $schema;
 
-	public function __construct($defaultSchema = 'RAW_DATA') {
-		$this->defaultSchema = $defaultSchema;
-	}
+    public function __construct($defaultSchema = 'RAW_DATA')
+    {
+        $this->defaultSchema = $defaultSchema;
+    }
 
-	public function onKernelRequest(GetResponseEvent $event) {
-		$request = $event->getRequest();
-		if (!$request->hasPreviousSession()) {
-			return;
-		}
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
 
-		// Detect the "SCHEMA" parameter in URL
-		$schema = $request->query->get('SCHEMA');
-		if (!empty($schema)) {
-			$request->getSession()->set('_schema', $schema);
-		} else {
-			// If no explicit schema has been set on this request, use one from the session
-			$schema = $request->getSession()->get('_schema', $this->defaultSchema);
-		}
-	}
+        // Detect the "SCHEMA" parameter in URL
+        $schema = $request->query->get('SCHEMA');
+        if (! empty($schema)) {
+            // Set the detected "SCHEMA" parameter
+            $this->schema = $request->getSession()->set('_schema', $schema);
+        } else {
+            // If no explicit schema has been set on this request
+            if ($request->hasPreviousSession()) {
+                // Use the "SCHEMA" parameter from the previous session
+                $this->schema = $request->getSession()->get('_schema', $this->defaultSchema);
+            } else {
+                $this->schema = $this->defaultSchema;
+            }
+        }
+    }
 
-	public static function getSubscribedEvents() {
-		return array(
-			// must be registered after the default Locale listener
-			KernelEvents::REQUEST => array(
-				array(
-					'onKernelRequest',
-					17
-				)
-			)
-		);
-	}
-	
-	public function getSchema()
-	{
-		return $this->schema;
-	}
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered after the default Locale listener
+            KernelEvents::REQUEST => array(
+                array(
+                    'onKernelRequest',
+                    17
+                )
+            )
+        );
+    }
+
+    public function getSchema()
+    {
+        return $this->schema;
+    }
 }
