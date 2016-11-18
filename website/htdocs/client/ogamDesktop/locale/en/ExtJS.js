b68c910b3fedbaedf656d7da60160e543f2733c2
florent bourcier <florent.bourcier@ign.fr>
2016-11-18 10:12:05 +0100

    OGAM-791:add H:i:s on altFromat for Ext.form.field.Time
    Former-commit-id: f1bbb3571d42a15da787c2caef1d03a68b2ac02f

--- website/htdocs/client/ogamDesktop/locale/en/ExtJS.js
+++ website/htdocs/client/ogamDesktop/locale/en/ExtJS.js
@@ -6,3 +6,12 @@ Ext.define("Ext.locale.en.panel.Panel", {
 	collapseToolText : 'Reduce',
 	expandToolText:'Extend'
 });
+Ext.Date.patterns = {
+    ShortTime: "g:i A",
+    LongTime: "g:i:s A"
+};
+Ext.define("Ext.locale.en.form.field.Time", {
+	override:'Ext.form.field.Time'
+}, function () {
+	Ext.form.field.Time.prototype.altFormats +="|H:i:s|"+Ext.Date.patterns.LongTime;
+});
