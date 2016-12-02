6756a849afa8565dde689915261ed81429b79a8b
Sylvain Galopin <Sylvain.Galopin@ign.fr>
2016-12-02 14:27:36 +0100

    OGAM-813 Rajouter des tests unitaires pour tester les droits
    Former-commit-id: c9e241aaa998ab0e50f480b091255c20e6ed58f8

---
+++ website/htdocs/server/ogamServer/tests/OGAMBundle/Controller/MapControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\OGAMBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class MapControllerTest extends AbstractControllerTest
+{
+    // *************************************************** //
+    //                 Access Right Tests                  //
+    // *************************************************** //
+    
+    /**
+     * Test access with a visitor login
+     */
+    public function testControllerActionVisitorAccess()
+    {
+        $this->logIn('visitor', array('ROLE_VISITOR'));
+        $this->checkControllerActionAccess($this->getVisitorUrls(), Response::HTTP_OK);
+    }
+    
+    public function getNotLoggedUrls(){
+        return $this->getAdminUrls();
+    }
+    
+    public function getVisitorUrls(){
+        return $this->getAdminUrls();
+    }
+    
+    public function getAdminUrls(){
+        return [
+            'getMapParametersAction' => [[
+                'uri' => '/map/get-map-parameters'
+            ]],
+            'ajaxgetlayertreenodesAction' => [[
+                'uri' => '/map/ajaxgetlayertreenodes'
+            ],[
+                'isJson' => true
+            ]]
+        ];
+    }
+}
