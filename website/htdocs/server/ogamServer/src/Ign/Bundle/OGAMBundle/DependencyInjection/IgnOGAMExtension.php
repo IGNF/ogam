c2f50802812edce8ce518a143744beebe7c5973e
Gautam <gautam.pastakia@ign.fr>
2016-12-28 13:48:32 +0100

    OGAM-832 : Chargement du services.yml depuis le bundle
    Former-commit-id: 35e18d7dc6f28c6091d94b8f99d51861a03b9386

--- website/htdocs/server/ogamServer/src/Ign/Bundle/OGAMBundle/DependencyInjection/IgnOGAMExtension.php
+++ website/htdocs/server/ogamServer/src/Ign/Bundle/OGAMBundle/DependencyInjection/IgnOGAMExtension.php
@@ -22,9 +22,7 @@ class IgnOGAMExtension extends Extension {
 		$configuration = new Configuration();
 		$config = $this->processConfiguration($configuration, $configs);
 
-		$loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-		$loader->load('services.xml');
-
-		$container->setParameter('ign_configurateur.database_name', $config['database_name']);
+		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+		$loader->load('services.yml');
 	}
 }
