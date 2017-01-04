a0afd5cfc557f9429194f7fa4824c7153fe47511
Severine <severine.candelier@gmail.com>
2017-01-04 15:36:23 +0100

    JIRA-:Revert the project to Symfony 2.8 Directory structure was still the one of a 3.x project
    Former-commit-id: 3ae38c10e467df4321b70599fcfe46d5452b986d

--- website/htdocs/server/ogamServer/web/app_dev.php
+++ website/htdocs/server/ogamServer/web/app_dev.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
+// read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
 // for more information
 //umask(0000);
 
@@ -15,9 +15,7 @@ if (!(in_array(@$_SERVER['SERVER_ADDR'], ['127.0.0.1', '192.168.50.4', 'fe80::1'
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-/**
- * @var Composer\Autoload\ClassLoader $loader
- */
+/** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
