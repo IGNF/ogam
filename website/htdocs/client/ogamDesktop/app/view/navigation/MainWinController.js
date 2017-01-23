553b9e279ce56b667c979c2a9e69deac59d3bc46
Sylvain Galopin <Sylvain.Galopin@ign.fr>
2017-01-23 10:23:40 +0100

    OGAM-831 Pb de l'impression qui fonctionne sous Chrome mais pas sous Firefox
    Former-commit-id: 503f3f412afe5be926ea844bf7020e807ede33e1

--- website/htdocs/client/ogamDesktop/app/view/navigation/MainWinController.js
+++ website/htdocs/client/ogamDesktop/app/view/navigation/MainWinController.js
@@ -25,16 +25,31 @@ Ext.define('OgamDesktop.view.navigation.MainWinController', {
         // Adds the current tab body content to the print preview div
         document.getElementById('o-navigation-print-main-div').appendChild(tab.body.dom.firstElementChild);
 
+        var afterPrint = function () {
+            var navBody = document.getElementById('o-navigation-print-main-div').firstElementChild;
+            tab.body.dom.appendChild(navBody);
+            Ext.get('o-navigation-print-main-div').destroy();
+        };
+
         // Adds a event to close properly the print window
-        if ('matchMedia' in window) {
-            window.matchMedia('print').addListener(function (mediaQueryListEvent) {
-                if (!mediaQueryListEvent.matches) { // After print
-                    var navBody = document.getElementById('o-navigation-print-main-div').firstElementChild;
-                    tab.body.dom.appendChild(navBody);
-                    Ext.get('o-navigation-print-main-div').destroy();
-                    mediaQueryListEvent.target.removeListener(arguments.callee);
-                }
-            });
+        // https://www.tjvantoll.com/2012/06/15/detecting-print-requests-with-javascript/
+        if (Ext.isWebKit) { // Chrome, Safari...
+            if ('matchMedia' in window) {
+                window.matchMedia('print').addListener(function (mediaQueryListEvent) {
+                    // Note: mediaQueryListEvent.matches will be true before printing, and false afterwards.
+                    if (!mediaQueryListEvent.matches) { // After print
+                        afterPrint();
+                        mediaQueryListEvent.target.removeListener(arguments.callee);
+                    }
+                });
+            }
+        } else if(Ext.isIE || Ext.isGecko) { // IE et Firefox
+            window.onafterprint = function(){ // After print
+                afterPrint();
+                window.onafterprint = null;
+            }
+        } else { // Opera...
+            // TODO: Add a close button like in the map print function
         }
 
         // Launches the print
